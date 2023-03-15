3186d8e295e5cc207c1f87d986f1bb43f89da3be
RUTHVIK <93712225+RuthvikKatta@users.noreply.github.com>
2023-03-15 15:09:31 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,67 +1,151 @@
+// Global Variables
 var numbers = new Array(50);
 var total = 1;
 
-Generate(0,0,0);
 
-function Generate(flag,index,green){
-    let bars = document.querySelector(".bars"); 
+GenerateIntial();
+
+function GenerateIntial() {
+    let bars = document.querySelector(".bars");
     while (bars.firstChild) {
         bars.removeChild(bars.firstChild);
     }
-    if(flag === 0){
-        for (var i = 0; i < numbers.length; i++) {
-            numbers[i] = Math.floor(Math.random() * (400-1+1)+1);
-        }
-    }
     for (var i = 0; i < numbers.length; i++) {
+        numbers[i] = Math.floor(Math.random() * (400 - 5) + 5);
         let child = document.createElement("div");
         child.classList.add("bar");
         child.setAttribute('data-value', `${numbers[i]}`);
         child.innerHTML = `<p>${numbers[i]}</p>`
         child.style.height = numbers[i] + "px";
-        if(index != 0 && index === i)
-            child.style.background = "rgb(240, 41, 41)";
-        else    
-            child.style.background = "white";
-        if(green-1 === i)
-            child.style.background = "rgb(72, 250, 120)";
         bars.appendChild(child);
     }
 }
 
-document.getElementById('generate').addEventListener('click', function(){
-    if(total == 1){
-        Generate(0,0,0);
+document.getElementById('generate').addEventListener('click', function () {
+    if (total == 1) {
+        GenerateIntial();
         total = 1;
     }
 });
 
 
+
 document.getElementById('Sort').addEventListener('click', () => {
-    var i = 1;
-    
-    function myLoop() {
-        var j = insertionSort(i);
-        Generate(1,i,j);
-        setTimeout(function () {
-            i++;
-            if (i < numbers.length) {
-                myLoop();
-            }
-            else {
-                sorted();
-                total = 1;
-            }
-        }, 250)
+
+    // Options Selection 
+
+    let select = document.getElementById("Algos");
+    Algorithm = select.value;
+
+    if (Algorithm === "InsertionSort") {
+        var i = 1;
+        let parent = document.querySelector(".bars");
+        function myLoop() {
+            insertionSort(i);
+            setTimeout(function () {
+                i++;
+                while (parent.firstChild) {
+                    parent.removeChild(parent.firstChild);
+                }
+                for (var j = 0; j < numbers.length; j++) {
+                    let child = document.createElement("div");
+                    child.classList.add("bar");
+                    child.setAttribute('data-value', `${numbers[j]}`);
+                    child.innerHTML = `<p>${numbers[j]}</p>`
+                    child.style.height = numbers[j] + "px";
+                    parent.appendChild(child);
+                }
+                if (i < numbers.length) {
+                    myLoop();
+                }
+                else {
+                    sorted();
+                    total = 1;
+                }
+            }, 250)
+        }
+        if (total == 1) {
+            total = 0;
+            myLoop();
+        }
     }
-    if(total == 1){
-        total = 0;
-        myLoop();
+
+    else if (Algorithm === "SelectionSort") {
+        var i = 0;
+        function myLoop() {
+            selectionsort(i);
+            setTimeout(function () {
+                i++;
+                let bars = document.querySelector(".bars");
+                while (bars.firstChild) {
+                    bars.removeChild(bars.firstChild);
+                }
+                for (var j = 0; j < numbers.length; j++) {
+                    let child = document.createElement("div");
+                    child.classList.add("bar");
+                    child.setAttribute('data-value', `${numbers[j]}`);
+                    child.innerHTML = `<p>${numbers[j]}</p>`
+                    child.style.height = numbers[j] + "px";
+                    bars.appendChild(child);
+                }
+                if (i < numbers.length) {
+                    myLoop();
+                }
+                else {
+                    sorted();
+                    total = 1;
+                }
+            }, 500)
+        }
+        if (total == 1) {
+            total = 0;
+            myLoop();
+        }
+    }
+
+    else if (Algorithm === "BubbleSort") {
+        var i = 0;
+        function myLoop() {
+            bubblesort(i);
+            setTimeout(function () {
+                i++;
+                if (i < numbers.length) {
+                    let bars = document.querySelector(".bars");
+                    while (bars.firstChild) {
+                        bars.removeChild(bars.firstChild);
+                    }
+                    for (var j = 0; j < numbers.length; j++) {
+                        let child = document.createElement("div");
+                        child.classList.add("bar");
+                        child.setAttribute('data-value', `${numbers[j]}`);
+                        child.innerHTML = `<p>${numbers[j]}</p>`
+                        child.style.height = numbers[j] + "px";
+                        bars.appendChild(child);
+                    }
+                    myLoop();
+                }
+                else {
+                    sorted();
+                    total = 1;
+                }
+            }, 500)
+        }
+        if (total == 1) {
+            total = 0;
+            myLoop();
+        }
     }
 })
 
+// Insertion Sort
+
+var g;
 function insertionSort(i) {
-    var g = -1;
+    let bars = document.querySelectorAll('.bar');
+    bars[i].style.background = "red";
+    if (g>=0 && g != i)
+        bars[g-1].style.background = "rgb(72, 250, 120)";
+    g = -1;
     for (var j = i; j > 0; j--) {
         if (numbers[j] < numbers[j - 1]) {
             var temp = numbers[j];
@@ -70,12 +154,67 @@ function insertionSort(i) {
             g = j;
         }
     }
-    return g;
 }
 
-function sorted(){
+// Bubble Sort
+
+function bubblesort(i) {
     let bars = document.querySelectorAll('.bar');
-    for(let one of bars){
+    var j = 0; 
+    function bubbleloop() {
+        if (numbers[j] > numbers[j + 1]) {
+            var temp = numbers[j];
+            numbers[j] = numbers[j + 1];
+            numbers[j + 1] = temp;
+        }
+        bars[(numbers.length - i - 1) - j].style.background = "rgb(72, 250, 120)";
+        bars[numbers.length - i - 1].style.background = "red";
+        setTimeout(function () {
+            j++;
+            if (j < numbers.length - i) {
+                bubbleloop();
+            }
+        }, 5)
+    }
+    bubbleloop();
+}
+
+// Selection Sort
+
+function selectionsort(i) {
+    var min = numbers[i];
+    var min_index = i;
+    var j = i + 1;
+    let bars = document.querySelectorAll('.bar');
+    bars[i].style.background = "red";
+    function Selectionloop() {
+        if (j < numbers.length)
+            bars[j].style.background = "rgb(72, 250, 120)";
+        if (min > numbers[j]) {
+            min = numbers[j];
+            min_index = j;
+        }
+        setTimeout(function () {
+            j++;
+            if (j < numbers.length) {
+                Selectionloop();
+            } else {
+                var temp = numbers[min_index];
+                numbers[min_index] = numbers[i];
+                numbers[i] = temp;
+                bars[min_index].style.background = "blue";
+                return min_index;
+            }
+        }, 5)
+    }
+    Selectionloop();
+}
+
+// Sorted
+
+function sorted() {
+    let bars = document.querySelectorAll('.bar');
+    for (let one of bars) {
         one.style.background = "white";
     }
     var i = 0;
@@ -86,11 +225,11 @@ function sorted(){
             if (i < numbers.length) {
                 myLoopgreen();
             }
-            else{
+            else {
                 i = 0;
                 myLoopwhite();
             }
-        }, 10)
+        }, 2.5)
     }
     myLoopgreen();
     function myLoopwhite() {
@@ -100,7 +239,7 @@ function sorted(){
             if (i < numbers.length) {
                 myLoopwhite();
             }
-        }, 10)
+        }, 2.5)
     }
 }
 
@@ -120,4 +259,4 @@ var slideUp = {
 
 ScrollReveal().reveal('#Title', slideDown);
 ScrollReveal().reveal('.buttons', slideUp);
-ScrollReveal().reveal('.visualizer', { scale: 0.85,duration:1200});
+ScrollReveal().reveal('.visualizer', { scale: 0.85, duration: 1200 });
